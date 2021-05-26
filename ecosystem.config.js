d3cba33a095dcf51e9d6d1b0509c4cf713ab10d1
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-27 00:57:52 +0700

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name: 'lotapi',
+    script: 'index.js',
+    error_file : "./err.log",
+    out_file : "./out.log",
+    watch : '.',
+    ignore_watch: ["node_modules","tmp","err.log",".env","Procfile","out.log"]
+  }],
+
+  deploy : {
+    production : {
+      user : 'pi',
+      host : 'raspberrypi',
+      ref  : 'origin/master',
+      repo : 'git@github.com:boyphongsakorn/lottsanook-nodejs.git',
+      path : '.',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
